1fc299dc12f83df888bf2667897f61020f16915c
Srilakshmi0216 <115430803+Srilakshmi0216@users.noreply.github.com>
2022-11-05 09:58:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,109 +1,73 @@
 class main
 {
-    constructor(FName1,LName1,Email,pd,ReEnterpd,Ptext)
+    constructor(fname,lname,uname,pwd)
     {
-
-    this.FN=FName1;
-    this.LN=LName1;
-    this.email=Email;
-    this.PWD=pd;
-    this.Password=ReEnterpd;
-    this.Nts=Ptext;
+    this.FN=fname;
+    this.LN=lname;
+    this.UN=uname;
+    this.Pwd=pwd;
 
     }
     getFN(){
         return this.FN;
-    
     }
     getLN(){
         return this.LN;
     }
-    getemail(){
-        return this.email;
-
+    getUN(){
+        return this.UN;
     }
-    getPWD()
+    getPwd()
     {
-        return this.PWD;
-
+        return this.Pwd;
     }
-    getPassword()
+    setFN(fname){
+        this.FN=fname;
+    }
+    setLN(lname){
+        this.LN=lname;
+    }      
+    setUN(uname){
+        this.UN=uname;
+    }
+    setPwd(pwd)
     {
-        return this.Password;
-
-
+        this.Pwd=pwd;
     }
-    getNts()
-    
-    {
-        return this.Nts;
-    }
-
-    setFN(FName1){
-        this.FN=FName1;
-    }
-    setLN(LName1){
-        this.LN=LName1;
-    }
-    setemail(Email){
-        this.email=Email;
-
-    }
-    setPWD(pd){
-        this.pd=PWD;
-    }
-    setPassword(ReEnterpdpd){
-        this.ReEnter=Password;
-    }
-    setNts(Ptext){
-        this.Ptext=Nts;
-    }
-
 }
-let create= document.getElementById("login-form");
-if(create) create.addEventListener('submit',login)
+const register=document.getElementById("formreg");
+if(register) register.addEventListener('submit',registertion)
+function registertion(e){
+    e.preventDefault();
+    let firstname=document.getElementById('fname').value;
+    let lastname=document.getElementById('lname').value;
+    let username=document.getElementById('uname').value;
+    let passwrd=document.getElementById('password').value;
 
-function login(e){
- e.preventDefault();
- let em1=document.getElementById("email").value;
- let pwd=document.getElementById("Password").value;
-
- let luser=new bala(em1,pwd);
- console.log(`${em1}`);
- console.log(`${pwd}`);
- create.reset();
-
+    let regi= new main(firstname,lastname,username,passwrd);
+    console.log(regi.FN)
+    console.log(regi.LN)
+    console.log(regi.UN)
+    console.log(regi.Pwd)
+    register.reset();
+}
+const loginform=document.getElementById("login");
+if(loginform) loginform.addEventListener('submit', loginuser)
+function loginuser(l){
+    l.preventDefault();
+    let user=document.getElementById('uname').value;
+    let password=document.getElementById('pasword').value;
+    console.log(`${user}`);
+    console.log(`${password}`);
+    loginform.reset();
 }
 
-let Reg=document.getElementById("regsiter-form");
-if(Reg) Reg.addEventListener('submit',breg)
-
-function breg(r){
-    r.preventDefault();
-    let FU1= document.getElementById("FN").value;
-    let Lr=document.getElementById("LN").value;
-    let eml1=document.getElementById("email").value;
-    let pwd=document.getElementById("PWD").value;
-    let pset=document.getElementById("Password").value;
-    
-    //let regi= new bala(FU1,Lr,eml1,pwd,pset);
-    console.log(`${FU1}`);
-    console.log(`${Lr}`);
-    console.log(`${eml1}`);
-    console.log(`${pwd}`);
-    console.log(`${pset}`);   
-    Reg.reset();
-    
-}
-
-let UserN= document.getElementById("note-form");
-if(UserN) UserN.addEventListener('submit',page)
-
-function page(b){
-    b.preventDefault();
-    let Notepage= document.getElementById("Nts").value;
-    //let usr= new bala(Notepage);
-    console.log(`${Notepage}`);
-    UserN.reset();
-
+const noteform=document.getElementById("note");
+if(noteform) noteform.addEventListener('submit',notem)
+function notem(f)
+{
+    f.preventDefault();
+    let notetext=document.getElementById('noteid').value;
+    console.log(`${notetext}`);
+    noteform.reset();
 }
